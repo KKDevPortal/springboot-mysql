a7b12859f3023da97594763cbc740fcb29d6d798
Kaushal Kishor <kaushal.kishor@niit.com>
2025-12-30 13:21:10 +0530

    init project

---
+++ src/test/java/com/kkdevportal/springboot_mysql/SpringbootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.springboot_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
